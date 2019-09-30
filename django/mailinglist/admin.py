db1c4b57d834217d4f14a1d990f8d42764c0f1da
Rakibul <mrakibul70@gmail.com>
2019-09-30 18:54:18 +0600

    Update model and database

--- django/mailinglist/admin.py
+++ django/mailinglist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MailingList, Message, Subscriber
 
-# Register your models here.
+
+admin.site.register(MailingList)
+admin.site.register(Message)
+admin.site.register(Subscriber)
